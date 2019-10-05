08e775a7f4e5ef3c252d83d9820001a2364113d6
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-10-05 10:33:05 -0400

    created a file for folder service test

---
+++ test/folder-service.spec.js
@@ -0,0 +1,9 @@
+const FolderService = require('../src/folder-functionality/folder-service');
+const knex = require('knex');
+const app = require('../src/app');
+const { makeFoldersArray } = require('./fixtures/folder-fixtures');
+const { makeNotesArray } = require('./fixtures/note-fixtures');
+
+describe(`FOLDERS SERVICE TEST`, ()=>{
+    
+});
