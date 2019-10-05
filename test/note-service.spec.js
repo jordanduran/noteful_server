aaae7f94ee35ee429188b9fc0b0e1fbca2880c2a
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-10-05 13:43:17 -0400

    added tests for notebyId and deleteNote

--- test/note-service.spec.js
+++ test/note-service.spec.js
@@ -40,29 +40,70 @@ describe.only(`Notes Service`, ()=>{
             })
         
         })
+        
         beforeEach(`add folder`, ()=>{
             return db(FOLDERS_TABLE)
             .insert(testFolders)
         })
+
         it(`add new note`, ()=>{
-            // this.retries(4);
             const newNote ={
                 note_name: 'test note name',
                 content: 'note note note',
+                date_modified: new Date(),
                 folder_id: 1
             }
             return NotesService.addNote(db, newNote)
                 .then((actualNote) => {
-                    console.log(testFolders)
                     expect(actualNote).to.eql({
                         id: 1,
                         note_name: newNote.note_name,
                         content: newNote.content,
-                        date_modified: new Date(),
+                        date_modified: newNote.date_modified,
                         folder_id: newNote.folder_id
                     })
                 })
         })
-    
+    });
+
+    context(`Given there is data in notes and folders table`, ()=>{
+        
+        beforeEach(`Insert data into folders`, ()=>{
+            return db(FOLDERS_TABLE)
+                .insert(testFolders);
+        })
+
+        beforeEach(`Insert data into notes`, ()=>{
+            return db(NOTES_TABLE)
+                .insert(testNotes);
+        })
+
+        it(`getAllNotes() - return all notes in the table`, ()=>{
+            return NotesService.getAllNotes(db)
+                .then((notes) => {
+                    expect(notes).to.deep.eql(testNotes);
+                });
+        });
+
+        it(`getNoteById() - returns the correct note`, ()=>{
+            const id = 1;
+            const expectedNote = testNotes[id-1];
+            return NotesService.getNotesById(db, id)
+                .then((actualNote) => {
+                    expect(actualNote).to.deep.eql(expectedNote);
+                })
+        });
+
+        it(`deleteNote() - deletes the correct note`, ()=>{
+            const id = 3;
+            const expectedNotes = testNotes.filter((note) => note.id !== id)
+            return NotesService.deleteNote(db, id)
+                .then((res) => {
+                    return NotesService.getAllNotes(db)
+                        .then((actualNotes) => {
+                            expect(expectedNotes).to.deep.eql(actualNotes);
+                        });
+                })
+        });
     })
 })
